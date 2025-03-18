1b68e48e898ba20c3d17c75103120a20eb78c741
dariusbakunas <bakunas@gmail.com>
2025-03-18 09:45:27 -0400

    feat: add daemon option

--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,21 @@
 use clap::Parser;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 #[derive(Parser, Debug)]
 #[command(version = env!("APP_VERSION"), about, long_about = None)]
 pub struct Cli {
-    #[clap(short, long)]
+    #[cfg(unix)]
+    #[arg(short, long, help = "Run as a daemon process.")]
+    pub daemon: bool,
+
+    #[arg(
+        short = 'l',
+        long,
+        default_value = "127.0.0.1:1080",
+        help = "Listen address for the SOCKS server."
+    )]
     pub listen_addr: String,
 
-    #[arg(long, value_name = "FILE", value_parser)]
+    #[arg(short = 'r', long, help = "Path to the routing configuration file.")]
     pub route_config: PathBuf,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,67 @@
+use std::env;
+
+#[cfg(unix)]
+use daemonize::Daemonize;
+
+#[cfg(unix)]
+use std::fs::File;
+
+#[cfg(unix)]
+use tokio::signal::unix::{signal, SignalKind};
+
 use anyhow::Result;
 use clap::Parser;
 use socks_router::cli::Cli;
 use socks_router::socks5::server::spawn_socks_server;
+use std::path::PathBuf;
 use tokio::signal;
 use tokio::sync::watch;
 
+#[cfg(unix)]
+fn get_temp_file_path(file_name: &str) -> PathBuf {
+    let mut temp_dir = env::temp_dir(); // Get system temporary directory
+    temp_dir.push(file_name); // Append the file name
+    temp_dir
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    #[cfg(unix)]
+    {
+        if cli.daemon {
+            let stdout_path = get_temp_file_path("socks_router.stdout.log");
+            let stderr_path = get_temp_file_path("socks_router.stderr.log");
+            let pid_path = get_temp_file_path("socks_router.pid");
+
+            // File descriptors for logging daemon output
+            let stdout = File::create(&stdout_path)?;
+            let stderr = File::create(&stderr_path)?;
+
+            let daemon = Daemonize::new()
+                .pid_file(&pid_path) // Write the process PID to this file
+                .stdout(stdout.try_clone()?) // Redirect stdout
+                .stderr(stderr.try_clone()?); // Redirect stderr
+
+            println!(
+                "Socks-Router Daemon: \nstdout: {}\nstderr: {}\npid: {}",
+                stdout_path.display(),
+                stderr_path.display(),
+                pid_path.display()
+            );
+
+            daemon.start().expect("Failed to daemonize process");
+        }
+    }
+
+    tokio_main(cli)
+}
+
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn tokio_main(cli: Cli) -> Result<()> {
     env_logger::init_from_env(
         env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
     );
-
-    let cli = Cli::parse();
 
     let (shutdown_tx, shutdown_rx) = watch::channel(false);
 
@@ -25,10 +75,29 @@ async fn main() -> Result<()> {
 }
 
 pub async fn handle_shutdown_signal(shutdown_tx: watch::Sender<bool>) {
-    // Wait for a shutdown signal (e.g., Ctrl+C)
-    signal::ctrl_c()
-        .await
-        .expect("Failed to listen for shutdown signal");
+    #[cfg(unix)]
+    let mut sigterm = signal(SignalKind::terminate()).expect("Failed to listen for SIGTERM");
+
+    #[cfg(unix)]
+    let sigterm_recv = sigterm.recv();
+
+    let ctrl_c = signal::ctrl_c();
+
+    #[cfg(unix)]
+    tokio::select! {
+        // Wait for Ctrl+C (cross-platform shutdown signal)
+        _ = ctrl_c => {
+            log::info!("Received Ctrl+C, shutting down...");
+        }
+
+        // Wait for SIGTERM (Unix-specific)
+        _ = sigterm_recv => {
+            log::info!("Received SIGTERM, shutting down...");
+        }
+    }
+
+    #[cfg(not(unix))]
+    ctrl_c.await.expect("Failed to listen for shutdown signal");
 
     // Send the shutdown notification to all receivers
     let _ = shutdown_tx.send(true);
